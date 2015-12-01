4d447b15722afa5463c743f75808a9ba422a93ed
ReenExe <ipourri@gmail.com>
2015-12-01 23:06:01 +0200

    simple blank test

---
+++ src/DesignPatternGenerator/DesignPatternGenerator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ReenExe\DesignPatternGenerator;
+
+class DesignPatternGenerator
+{
+
+}
---
+++ tests/DesignPatternGeneratorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use ReenExe\DesignPatternGenerator\DesignPatternGenerator;
+
+class DesignPatternGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        new DesignPatternGenerator();
+    }
+}
