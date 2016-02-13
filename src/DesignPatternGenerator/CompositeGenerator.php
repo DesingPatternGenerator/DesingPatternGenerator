9934ea3bc604c6a647096c0eb09158fd48343b7d
ReenExe <ipourri@gmail.com>
2016-02-13 20:55:48 +0200

    progress

--- src/DesignPatternGenerator/CompositeGenerator.php
+++ src/DesignPatternGenerator/CompositeGenerator.php
@@ -6,6 +6,39 @@ class CompositeGenerator extends Generator
 {
     public function generate(array $settings): bool
     {
-        return false;
+        $class = $settings['class'];
+
+        $reflection = new \ReflectionClass($class);
+
+        if ($reflection->isFinal()) {
+            return false;
+        }
+
+        $this
+            ->clearUse()
+            ->addUseClass($class);
+
+        $sourceClassMethods = $this->getClassMethods($reflection);
+
+        if (empty($sourceClassMethods)) {
+            return false;
+        }
+
+        $sourceClassName = $reflection->getShortName();
+        $namespace = $settings['namespace'];
+        $path = $settings['path'];
+        $resultClassName = $sourceClassName . 'Composite';
+
+        $methods = $this->getClassMethods($reflection);
+
+        $result = $this->getResultClassString([
+            ':namespace:' => "namespace $namespace;",
+            ':header:' => "class $resultClassName {$this->getBehavior($reflection)} $sourceClassName",
+            ':body:' => join(PHP_EOL, $methods),
+        ]);
+
+        $this->store($path, $resultClassName, $result);
+
+        return true;
     }
 }
