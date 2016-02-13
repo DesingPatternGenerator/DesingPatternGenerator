b909a6ee6888afb4294068bb946038b9fed1238b
ReenExe <ipourri@gmail.com>
2016-02-13 21:11:02 +0200

    progress

--- tests/DesignPattern/CompositeTest.php
+++ tests/DesignPattern/CompositeTest.php
@@ -7,33 +7,37 @@ use ReenExe\DesignPatternGenerator\CompositeGenerator;
 use ReenExe\Fixtures\Source\View;
 use ReenExe\Fixtures\Source\ViewInterface;
 
+use ReenExe\Fixtures\Result\Composite\ViewComposite;
+use ReenExe\Fixtures\Result\Composite\ViewInterfaceComposite;
+
 class CompositeTest extends AbstractReflectionTest
 {
     /**
-     * @dataProvider dataProvider
-     * @param $sourceClassName
+     * @var CompositeGenerator
      */
-    public function testViewComposite($sourceClassName)
-    {
-        $generator = new CompositeGenerator();
+    private $generator;
 
-        $generator->generate([
+    protected function setUp()
+    {
+        $this->generator = new CompositeGenerator();
+    }
+
+    public function testViewComposite()
+    {
+        $this->assertTrue($this->generate(View::class));
+    }
+
+    public function testViewInterfaceComposite()
+    {
+        $this->assertTrue($this->generate(ViewInterface::class));
+    }
+
+    private function generate($sourceClassName)
+    {
+        $this->generator->generate([
             'class' => $sourceClassName,
             'namespace' => 'ReenExe\Fixtures\Result\Composite',
             'path' => FIXTURE_RESULT_PATH . '/Composite',
         ]);
-    }
-
-    public function dataProvider()
-    {
-        $methods = [];
-
-        yield [
-            View::class
-        ];
-
-        yield [
-            ViewInterface::class
-        ];
     }
 }
